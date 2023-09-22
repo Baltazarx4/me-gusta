6af6f266c208976c8bc0b312b765edbe45affe43
Baltazarx4 <baltazarjuanandrescifuentes@gmail.com>
2023-09-21 23:58:12 -0300

    Me gusta

---
+++ script.js
@@ -0,0 +1,21 @@
+function like1(){
+
+    let elementocontadordelikess= document.querySelector("#contadordelikes");
+    let contadordelikes= elementocontadordelikess.innerText;
+    contadordelikes ++;
+    elementocontadordelikess.innerText= contadordelikes;
+}
+function like2(){
+
+    let elementocontadordelikess= document.querySelector("#contadordelikes2");
+    let contadordelikes= elementocontadordelikess.innerText;
+    contadordelikes ++;
+    elementocontadordelikess.innerText= contadordelikes;
+}
+function like3(){
+
+    let elementocontadordelikess= document.querySelector("#contadordelikes3");
+    let contadordelikes= elementocontadordelikess.innerText;
+    contadordelikes ++;
+    elementocontadordelikess.innerText= contadordelikes;
+}
